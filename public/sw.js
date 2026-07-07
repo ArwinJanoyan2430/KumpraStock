6b4f26caccc6154ebcd61b23688403f88cdb059d
Arwin Ryan C. Janoyan <arwintiktok24@gmail.com>
2026-07-08 01:37:24 +0800

    add offline support

--- public/sw.js
+++ public/sw.js
@@ -1,58 +1,44 @@
 const CACHE_NAME = "kumpra-stock-v1";
 
-const APP_SHELL = ["/", "/index.html"];
-
 self.addEventListener("install", (event) => {
-  console.log("Service Worker installing...");
-
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(APP_SHELL);
-    }),
-  );
+  console.log("Service Worker installing");
 
   self.skipWaiting();
 });
 
+
 self.addEventListener("activate", (event) => {
-  console.log("Service Worker activated...");
+  console.log("Service Worker activated");
 
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key)),
-      );
-    }),
-  );
-
-  self.clients.claim();
+  event.waitUntil(self.clients.claim());
 });
+
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
+    caches.match(event.request)
+      .then((cached) => {
 
-      return fetch(event.request)
-        .then((response) => {
-          // Save files for offline use
-          if (response.status === 200 && response.type === "basic") {
+        if (cached) {
+          return cached;
+        }
+
+        return fetch(event.request)
+          .then((response) => {
+
             const clone = response.clone();
 
-            caches.open(CACHE_NAME).then((cache) => {
-              cache.put(event.request, clone);
-            });
-          }
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, clone);
+              });
 
-          return response;
-        })
-        .catch(() => {
-          return caches.match("/index.html");
-        });
-    }),
+            return response;
+          })
+          .catch(() => {
+            return caches.match("/");
+          });
+
+      })
   );
 });
